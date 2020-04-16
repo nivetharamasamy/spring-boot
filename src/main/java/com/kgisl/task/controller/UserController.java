90a60abc513b8dc17560949037f7d20a30e72005
nivetharamasamy <57795627+nivetharamasamy@users.noreply.github.com>
2020-04-16 14:22:42 +0530

    Update UserController.java

--- src/main/java/com/kgisl/task/controller/UserController.java
+++ src/main/java/com/kgisl/task/controller/UserController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
