290255d247467f15d738a6b126ab9d8ecd90657a
nivetharamasamy <57795627+nivetharamasamy@users.noreply.github.com>
2020-04-21 16:50:02 +0530

    Update ResRepo.java

--- src/main/java/com/kgisl/task/repository/ResRepo.java
+++ src/main/java/com/kgisl/task/repository/ResRepo.java
@@ -2,14 +2,14 @@ package com.kgisl.task.repository;
 
 import java.io.Serializable;
 
-import com.kgisl.task.entity.resources;
+import com.kgisl.task.entity.Resources;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * TeamRepository
  */
-public interface ResRepo extends JpaRepository<resources, Serializable> {
+public interface ResRepo extends JpaRepository<Resources, Serializable> {
 
     
 }
