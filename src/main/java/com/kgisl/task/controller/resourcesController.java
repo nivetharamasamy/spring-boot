f70dc008ae73e55930806f2c9f6ce839d997c1bb
nivetharamasamy <57795627+nivetharamasamy@users.noreply.github.com>
2020-04-21 16:48:32 +0530

    Update resourcesController.java

--- src/main/java/com/kgisl/task/controller/resourcesController.java
+++ src/main/java/com/kgisl/task/controller/resourcesController.java
@@ -2,10 +2,11 @@ package com.kgisl.task.controller;
 
 import java.util.List;
 
-import com.kgisl.task.entity.resources;
+import com.kgisl.task.entity.Resources;
+
 import com.kgisl.task.service.ResourcesService;
 
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +36,17 @@ public class resourcesController {
 
 
     @GetMapping("/")
-    public @ResponseBody ResponseEntity<List<resources>> all() {
+    public @ResponseBody ResponseEntity<List<Resources>> all() {
         return new ResponseEntity<>(resourcesservice.getRes(), HttpStatus.OK);
+    }
+
+    
+    @PostMapping(value = "/", headers = "Accept=application/json")
+    public ResponseEntity<Resources> createTeam(@RequestBody Resources res) {
+        Resources respost=resourcesservice.createres(res);
+        HttpHeaders headers = new HttpHeaders();
+        
+        return new ResponseEntity<>(respost,headers, HttpStatus.CREATED);
     }
 
 }
